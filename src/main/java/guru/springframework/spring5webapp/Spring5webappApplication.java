4fc3c3f80072e36f5ae17a841811cd6260387666
danielyahn <danielyahn@gmail.com>
2019-11-19 19:55:39 -0500

    H2 Console with JDBC template

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -2,11 +2,21 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(Spring5webappApplication.class, args);
+
+		DataSource ds = (DataSource) ctx.getBean("dataSource");
+
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(ds);
+		int result = jdbcTemplate.queryForObject("SELECT count(*) FROM AUTHOR", Integer.class);
+		System.out.println("Query Result:"+result);
 	}
 }
---
+++ src/main/java/guru/springframework/spring5webapp/config/DataSourceConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+
+public class DataSourceConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("password");
+
+        return dataSource;
+    }
+}
